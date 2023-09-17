c68c9b3033f72ae502a8fc3b04620a41d479149b
Huixian-Gong <huixian@usc.edu>
2023-09-17 00:06:57 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,40 +1,48 @@
 from flask import Flask, render_template, request, redirect, url_for
-import keras.models
-import tensorflow as tf
-from tensorflow.keras.preprocessing import image
-import cv2
+import os
 import numpy as np
+from tensorflow.keras.models import model_from_json
+import cv2
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'static/images'
 
-# Load your TensorFlow CNN model
-model = tf.keras.models.load_model('/Users/huixian/Library/CloudStorage/OneDrive-UniversityofSouthernCalifornia/Hackathon/mindspark-hackathon-Huixian-Gong/new_model.h5')
+# Load the CNN model
+model = None
+
+with open('model/pothole_cnn.json', 'r') as json_file:
+    loaded_model_json = json_file.read()
+    model = model_from_json(loaded_model_json)
+    model.load_weights('model/model_weights.h5')
+
+# Define a function to preprocess the image before prediction
+def preprocess_image(image_path):
+    img = cv2.imread(image_path)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img = cv2.resize(img, (300, 300))
+    img = img / 255.0
+    img = np.expand_dims(img, axis=0)  # Add batch dimension
+    return img
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        # Handle image upload
-        if 'image' in request.files:
-            image_file = request.files['image']
-            if image_file.filename != '':
-                # Ensure the file is an image (you can add more validation)
-                if image_file.filename.endswith(('.jpg', '.jpeg', '.png')):
-
-                    print(type(image_file))
-                    print(request.path)
-                    # Load and preprocess the image using TensorFlow
-                    img = image.load_img(image_file.filename, target_size=(300, 300))  # Adjust target size as needed
-                    img = img[:, :, 0] / 255.0  # Normalize the image
-                    img = image.img_to_array(img)
-                    img = np.expand_dims(img, axis=0)
-                    img = tf.keras.applications.vgg16.preprocess_input(img)  # Adjust preprocessing for your model
-
-                    # Make predictions using your TensorFlow CNN model
-                    predictions = model.predict(img)
-                    # Process the predictions as needed
-
-                    return render_template('result.html', predictions=predictions)
-
+        if 'file' not in request.files:
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            return redirect(request.url)
+        if file:
+            image_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+            file.save(image_path)
+            image = preprocess_image(image_path)
+            prediction = model.predict(image)
+            predicted_class = np.argmax(prediction)
+            class_labels = ['normal', 'pothole']
+            predicted_label = class_labels[predicted_class]
+            # You can customize this part to display the prediction results as needed.
+            return render_template('result.html', prediction=predicted_label)
     return render_template('index.html')
 
 if __name__ == '__main__':
